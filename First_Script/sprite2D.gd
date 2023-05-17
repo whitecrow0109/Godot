c65c1ac4d37f52a4cd88498db0ee603efd10476d
whitecrow0109 <whitecrow0109@gmail.com>
2023-05-16 22:56:42 -0700

    Add signal via Code

--- First_Script/sprite2D.gd
+++ First_Script/sprite2D.gd
@@ -3,23 +3,21 @@ extends Sprite2D
 var speed = 400
 var angular_speed = PI
 
+
+func _ready():
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_Timer_timeout)
+
+
 func _process(delta):
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-
-	rotation += angular_speed * direction * delta
-
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
-	if Input.is_action_pressed("ui_down"):
-		velocity = Vector2.DOWN.rotated(rotation) * speed
-
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
 
 
 func _on_button_pressed():
 	set_process(not is_processing())
+
+
+func _on_Timer_timeout():
+	visible = not visible
